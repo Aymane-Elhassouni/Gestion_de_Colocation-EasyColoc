04656caeaf35fff17532fc7e81923805f5087a98
Aymane-Elhassouni <aymane.elhassouni3@gmail.com>
2026-03-04 10:41:29 +0000

    add modification

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,42 +34,20 @@ class RegisteredUserController extends Controller
             'lastname' => ['required', 'string', 'max:50'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'invitation_code' => ['required'], // Zid l-vifirication dyal l-code hna
         ]);
-
-        // 1. Vifiri wach l-code shih u dyal had l-email
-        $invitation = \App\Models\Invitation::where('token', $request->invitation_code)
-            ->where('email', $request->email)
-            ->first();
-
-        if (!$invitation) {
-            return back()->withErrors(['invitation_code' => 'Le code d\'invitation est incorrect ou ne correspond pas à cet email.']);
-        }
-
         $status = $request->boolean('status', true);
-
-        // User li katsajel b invitation code dima Role 1 (User)
+        $role_id = User::exists() ? 1 : 2;
         $user = User::create([
             'firstname' => $request->firstname,
             'lastname' => $request->lastname,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'status' => $status,
-            'role_id' => 1,
+            'role_id' => $role_id,
         ]);
-
-        // 2. Linkih m3a d-dar f t-table pivot (user_colocation)
-        $user->colocations()->attach($invitation->colocation_id, [
-            'role_colocation' => 'membre' // Aw ay smiya bghiti t-3ti l-had l-user f d-dar
-        ]);
-
-        // 3. Mssah l-invitation bach ma-t-sta3melch mara khra
-        $invitation->delete();
 
         event(new Registered($user));
         Auth::login($user);
-
-        // Redirection directe l-dashboard s-shih
         if ($user->role_id == 2) {
             return redirect()->route('admin.dashboard');
         }
